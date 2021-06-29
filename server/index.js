d7c30ab6b565118abb6a8a03561bb6437e737397
Jorge <jorgediazok@gmail.com>
2021-06-29 02:50:23 -0300

    some extra comments added

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+//THIS SERVER WAS CREATED BECAUSE OF THE CORS PROBLEM WHICH COULDNT BE RESOLVED IN FRONT
+
 const express = require('express');
 const cors = require('cors');
 const apiRoutes = require('./routes/apiRoutes');
@@ -9,5 +11,6 @@ app.use(cors());
 
 app.use('/', apiRoutes);
 
+//PORT CONNECTION
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
